cb2bee3ced39d31eb31df3dd115fd3021273aefb
ecunal <ecaliskanunal@gmail.com>
2022-10-20 15:37:20 +0100

    Driver edited for Jenkins

--- src/test/java/com/cydeo/stepDefinitions/OrderStepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/OrderStepDefinitions.java
@@ -23,19 +23,17 @@ public class OrderStepDefinitions {
     WebTablesPage webTablesPage = new WebTablesPage();
     BasePage basePage = new BasePage();
     OrderPage orderPage = new OrderPage();
-
     ViewAllOrdersPage viewAllOrdersPage = new ViewAllOrdersPage();
+    JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
+    Actions actions = new Actions(Driver.getDriver());
 
     @Given("user is on the order page")
     public void user_is_on_the_order_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("webTablesURL"));
-        webTablesPage.inputUsername.sendKeys("Test");
-        webTablesPage.inputPassword.sendKeys("Tester");
-        webTablesPage.loginButton.click();
+        webTablesPage.login();
         //webTablesPage.login(); //this method passes Test and tester as credentials, here we call the method to log in to the app
 
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", basePage.order);
+        //executor.executeScript("arguments[0].scrollIntoView(true);", basePage.order);
         //BrowserUtils.sleep(10);
 
 //        Actions actions = new Actions(Driver.getDriver());
@@ -51,11 +49,8 @@ public class OrderStepDefinitions {
     @When("user selects product type {string}")
     public void user_selects_product_type(String string) {
 
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.productDropdown);
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.productDropdown);
 
-        Actions actions = new Actions(Driver.getDriver());
-        //actions.doubleClick(basePage.clickBeforeAll).perform();
         actions.moveToElement(orderPage.productDropdown).perform();
         actions.click(orderPage.productDropdown).perform();
 
@@ -71,9 +66,9 @@ public class OrderStepDefinitions {
         //orderPage.inputQuantity.sendKeys(Keys.BACK_SPACE); // more accurate than .clear()
         //orderPage.quantity.sendKeys(string);
 
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.quantity);
-        Actions actions = new Actions(Driver.getDriver());
+
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.quantity);
+
         actions.click(orderPage.quantity).sendKeys(Keys.BACK_SPACE).sendKeys(string).perform();
 
         //JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
@@ -82,22 +77,19 @@ public class OrderStepDefinitions {
 
     @When("user enters name {string}")
     public void user_enters_name(String string) {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputName);
 
-        Actions actions = new Actions(Driver.getDriver());
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputName);
+
         actions.moveToElement(orderPage.inputName).perform();
         actions.click(orderPage.inputName).sendKeys(string).perform();
 
-        //orderPage.inputName.sendKeys(string);
     }
 
     @When("user enters street {string}")
     public void user_enters_street(String string) {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputStreet);
 
-        Actions actions = new Actions(Driver.getDriver());
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputStreet);
+
         actions.moveToElement(orderPage.inputStreet).perform();
         actions.click(orderPage.inputStreet).sendKeys(string).perform();
 
@@ -106,10 +98,9 @@ public class OrderStepDefinitions {
 
     @When("user enters city {string}")
     public void user_enters_city(String string) {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputCity);
 
-        Actions actions = new Actions(Driver.getDriver());
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputCity);
+
         actions.moveToElement(orderPage.inputCity).perform();
         actions.click(orderPage.inputCity).sendKeys(string).perform();
 
@@ -119,10 +110,9 @@ public class OrderStepDefinitions {
 
     @When("user enters state {string}")
     public void user_enters_state(String string) {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputState);
 
-        Actions actions = new Actions(Driver.getDriver());
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputState);
+
         actions.moveToElement(orderPage.inputState).perform();
         actions.click(orderPage.inputState).sendKeys(string).perform();
 
@@ -131,10 +121,9 @@ public class OrderStepDefinitions {
 
     @When("user enters zipcode {string}")
     public void user_enters_zipcode(String string) {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputZip);
 
-        Actions actions = new Actions(Driver.getDriver());
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputZip);
+
         actions.moveToElement(orderPage.inputZip).perform();
         actions.click(orderPage.inputZip).sendKeys(string).perform();
 
@@ -160,10 +149,9 @@ public class OrderStepDefinitions {
 
     @When("user enters card number {string}")
     public void user_enters_card_number(String string) {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputCardNo);
 
-        Actions actions = new Actions(Driver.getDriver());
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputCardNo);
+
         actions.moveToElement(orderPage.inputCardNo).perform();
         actions.click(orderPage.inputCardNo).sendKeys(string).perform();
 
@@ -172,10 +160,9 @@ public class OrderStepDefinitions {
 
     @When("user enters expiration date {string}")
     public void user_enters_expiration_date(String string) {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputCardExp);
 
-        Actions actions = new Actions(Driver.getDriver());
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.inputCardExp);
+
         actions.moveToElement(orderPage.inputCardExp).perform();
         actions.click(orderPage.inputCardExp).sendKeys(string).perform();
 
@@ -184,10 +171,9 @@ public class OrderStepDefinitions {
 
     @When("user clicks process order button")
     public void user_clicks_process_order_button() {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.processOrderButton);
 
-        Actions actions = new Actions(Driver.getDriver());
+        //executor.executeScript("arguments[0].scrollIntoView(true);", orderPage.processOrderButton);
+
         actions.moveToElement(orderPage.processOrderButton).perform();
         actions.click(orderPage.processOrderButton).perform();
 
@@ -196,8 +182,8 @@ public class OrderStepDefinitions {
 
     @Then("user should see {string} in the first row of the web table")
     public void user_should_see_in_the_first_row_of_the_web_table(String expectedCustomerName) {
-        JavascriptExecutor executor = (JavascriptExecutor) Driver.getDriver();
-        executor.executeScript("arguments[0].scrollIntoView(true);", viewAllOrdersPage.newCustomerCell);
+
+        //executor.executeScript("arguments[0].scrollIntoView(true);", viewAllOrdersPage.newCustomerCell);
         String actualCustomerName = viewAllOrdersPage.newCustomerCell.getText();
         Assert.assertEquals(expectedCustomerName, actualCustomerName);
     }
